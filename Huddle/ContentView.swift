dfb73160f43f7409a651fbc173fbf598be8abb41
salvoZero4 <salvo.scaravalle@gmail.com>
2026-02-17 11:35:14 +0100

    Initial Commit

---
+++ Huddle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Huddle
+//
+//  Created by Salvatore Scaravalle on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Huddle/HuddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleApp.swift
+//  Huddle
+//
+//  Created by Salvatore Scaravalle on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HuddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
