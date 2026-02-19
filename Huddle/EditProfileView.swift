7e70fcae250f2e64ff209fe1c7012db431fbdf50
salvoZero4 <salvo.scaravalle@gmail.com>
2026-02-19 17:00:27 +0100

    Aggiunta EditProfileView

---
+++ Huddle/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Hubble
+//
+//  Created by Salvatore Scaravalle on 19/02/26.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
