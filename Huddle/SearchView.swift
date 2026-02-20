5d3d7a5cd388383eb609ac5c777021faa70c8a5c
salvoZero4 <salvo.scaravalle@gmail.com>
2026-02-20 17:24:43 +0100

    oo

---
+++ Huddle/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Hubble
+//
+//  Created by Salvatore Scaravalle on 20/02/26.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
