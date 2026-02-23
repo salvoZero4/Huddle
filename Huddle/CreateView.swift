7806e699c962f786ac9ed30746b31d5c448a7414
salvoZero4 <salvo.scaravalle@gmail.com>
2026-02-23 11:17:22 +0100

    m

--- Huddle/CreateView.swift
+++ Huddle/CreateView.swift
@@ -87,167 +87,170 @@ struct CreateView: View {
     let facoltaIngegneristica = ["Select engineering","Computer", "Electrical", "Civil", "Mechanical"]
     
     var body: some View {
-        ScrollView {
-            Text("Create an Huddle")
-                .font(.system(size: 30)).bold()
-                .foregroundColor(.blue)
-                .padding()
-            VStack{
-                HStack {
-                    Text("Info")
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                        .padding(.leading, 5)
-                    Spacer()
-                }
-                VStack {
-                    Picker("engineering", selection: $engineering){
-                        ForEach(facoltaIngegneristica, id: \.self) {
-                            ingegneria in Text(ingegneria)
-                        }
+        VStack{
+            ScrollView {
+                Text("Create an Huddle")
+                    .font(.system(size: 30)).bold()
+                    .foregroundColor(.blue)
+                    .padding()
+                VStack{
+                    HStack {
+                        Text("Info")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                            .padding(.leading, 5)
+                        Spacer()
                     }
-                    Divider()
-                    if engineering == "Computer"{
-                        Picker("Subject", selection: $subject){
-                            ForEach(materieComputer, id: \.self) {
-                                materia in Text(materia)
-                            }.tint(.blue)
+                    VStack {
+                        Picker("engineering", selection: $engineering){
+                            ForEach(facoltaIngegneristica, id: \.self) {
+                                ingegneria in Text(ingegneria)
+                            }
                         }
-                    }else if(engineering == "Electrical"){
-                        Picker("Subject", selection: $subject){
-                            ForEach(materieElectrical, id: \.self) {
-                                materia in Text(materia)
-                            }.tint(.blue)
+                        Divider()
+                        if engineering == "Computer"{
+                            Picker("Subject", selection: $subject){
+                                ForEach(materieComputer, id: \.self) {
+                                    materia in Text(materia)
+                                }.tint(.blue)
+                            }
+                        }else if(engineering == "Electrical"){
+                            Picker("Subject", selection: $subject){
+                                ForEach(materieElectrical, id: \.self) {
+                                    materia in Text(materia)
+                                }.tint(.blue)
+                            }
+                        }else if(engineering == "Mechanical"){
+                            Picker("Subject", selection: $subject){
+                                ForEach(materieMechanical, id: \.self) {
+                                    materia in Text(materia)
+                                }.tint(.blue)
+                            }
+                        }else if(engineering == "Civil"){
+                            Picker("Subject", selection: $subject){
+                                ForEach(materieCivil, id: \.self) {
+                                    materia in Text(materia)
+                                }.tint(.blue)
+                            }
+                        }else{
+                            Picker("Subject", selection: $subject){
+                                ForEach(materieDisponibili, id: \.self) {
+                                    materia in Text(materia)
+                                }.tint(.blue)
+                            }
                         }
-                    }else if(engineering == "Mechanical"){
-                        Picker("Subject", selection: $subject){
-                            ForEach(materieMechanical, id: \.self) {
-                                materia in Text(materia)
-                            }.tint(.blue)
-                        }
-                    }else if(engineering == "Civil"){
-                        Picker("Subject", selection: $subject){
-                            ForEach(materieCivil, id: \.self) {
-                                materia in Text(materia)
-                            }.tint(.blue)
-                        }
-                    }else{
-                        Picker("Subject", selection: $subject){
-                            ForEach(materieDisponibili, id: \.self) {
-                                materia in Text(materia)
-                            }.tint(.blue)
-                        }
+                        
+                        
+                        Divider()
+                        TextField("Insert Huddle description", text: $description)
+                            .padding(.vertical, 5)
                     }
-                    
-                    
-                    Divider()
-                    TextField("Insert Huddle description", text: $description)
-                        .padding(.vertical, 5)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(25)
+                }
+                .padding(.horizontal)
+                VStack{
+                    HStack {
+                        Text("Place and Date")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                            .padding(.leading)
+                        Spacer()
+                    }
+                    VStack {
+                        Picker("building", selection: $building){
+                            ForEach(edificiDisponibili, id: \.self) {
+                                edificio in
+                                Text(edificio)
+                            }
+                        }
+                        .tint(.blue)
+                        Divider()
+                        TextField("Insert Huddle room: ", text: $room)
+                            .padding(.vertical, 5)
+                        Divider()
+                        DatePicker(selection: $date, label: {Text("Huddle date: ")} )
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(25)
+                    Button(action:{
+                        openURL(URL(string:"https://offertaformativa.unipa.it/offweb/public/aula/aulaCalendar.seam")!)
+                    }) {
+                        Text("Check free rooms")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(.systemOrange))
+                            .foregroundColor(.white)
+                            .cornerRadius(25)
+                    }
                 }
                 .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(25)
+                VStack{
+                    HStack {
+                        Text("Social group")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                            .padding(.leading)
+                        Spacer()
+                    }
+                    VStack {
+                        TextField("Insert Whatsapp Link:(Optional) ", text: $linkWhatsapp)
+                            .padding(.vertical, 5)
+                            .foregroundColor(.green)
+                        Divider()
+                        TextField("Insert Telegram Link:(Optional) ", text: $linkTelegram)
+                            .padding(.vertical, 5)
+                            .foregroundColor(.cyan)
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(25)
+                }
+                .padding()
+                
             }
-            .padding(.horizontal)
+            
             VStack{
-                HStack {
-                    Text("Place and Date")
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                        .padding(.leading)
-                    Spacer()
-                }
-                VStack {
-                    Picker("building", selection: $building){
-                        ForEach(edificiDisponibili, id: \.self) {
-                            edificio in
-                            Text(edificio)
-                        }
-                    }
-                    .tint(.blue)
-                    Divider()
-                    TextField("Insert Huddle room: ", text: $room)
-                        .padding(.vertical, 5)
-                    Divider()
-                    DatePicker(selection: $date, label: {Text("Huddle date: ")} )
-                }
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(25)
-                Button(action:{
-                    openURL(URL(string:"https://offertaformativa.unipa.it/offweb/public/aula/aulaCalendar.seam")!)
+                Button(action: {
+                    //logica del bottone
+                    var _newHuddle = Huddle(
+                        subject: subject,
+                        building: building,
+                        room: room,
+                        description: description,
+                        date: date,
+                        linkW: linkWhatsapp,
+                        linkT: linkTelegram,
+                        engineering: engineering,
+                        users: []
+                        
+                    )
+                    //Salvo qui i dati nell main
+                    //DatabaseManager.shared.saveHuddle(nuovo: nuovoGruppo)
+                    
+                    /* subject = ""
+                     building = ""
+                     room = ""
+                     linkWhatsapp = ""
+                     linkTelegram = "" */
+                    
+                    
+                    
                 }) {
-                    Text("Check free rooms")
-                        .frame(maxWidth: .infinity)
+                    Text("Create")
+                        .font(.title2).bold()
+                        .frame(maxWidth: 350)
                         .padding()
-                        .background(Color(.systemOrange))
+                        .background(Color(.blue))
                         .foregroundColor(.white)
                         .cornerRadius(25)
                 }
+                .padding(.bottom, 10)
+                
+                .background(.ultraThinMaterial)
             }
-            .padding()
-            VStack{
-                HStack {
-                    Text("Social group")
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                        .padding(.leading)
-                    Spacer()
-                }
-                VStack {
-                    TextField("Insert Whatsapp Link:(Optional) ", text: $linkWhatsapp)
-                        .padding(.vertical, 5)
-                        .foregroundColor(.green)
-                    Divider()
-                    TextField("Insert Telegram Link:(Optional) ", text: $linkTelegram)
-                        .padding(.vertical, 5)
-                        .foregroundColor(.cyan)
-                }
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(25)
-            }
-            .padding()
-            
-        }
-        
-        VStack{
-            Button(action: {
-                //logica del bottone
-                 var _newHuddle = Huddle(
-                 subject: subject,
-                 building: building,
-                 room: room,
-                 description: description,
-                 date: date,
-                 linkW: linkWhatsapp,
-                 linkT: linkTelegram,
-                 engineering: engineering,
-                 users: []
-                 
-                 )
-                //Salvo qui i dati nell main
-                //DatabaseManager.shared.saveHuddle(nuovo: nuovoGruppo)
-                
-                /* subject = ""
-                 building = ""
-                 room = ""
-                 linkWhatsapp = ""
-                 linkTelegram = "" */
-                
-                
-                
-            }) {
-                Text("Create")
-                    .font(.title2).bold()
-                    .frame(maxWidth: 350)
-                    .padding()
-                    .background(Color(.blue))
-                    .foregroundColor(.white)
-                    .cornerRadius(25)
-            }
-            
-            .background(.ultraThinMaterial)
         }
     }
 }
