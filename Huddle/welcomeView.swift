611c28b191ee6412b151c36000bb3f2b95e4d175
salvoZero4 <salvo.scaravalle@gmail.com>
2026-02-26 12:02:51 +0100

    la

---
+++ Huddle/welcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  welcomeView.swift
+//  Huddle
+//
+//  Created by Salvatore Scaravalle on 26/02/26.
+//
+
+import SwiftUI
+
+struct welcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    welcomeView()
+}
